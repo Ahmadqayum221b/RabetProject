a999235501d4fba32fd17ed0bc55d6f04995f8e4
Ahmadqayum221b <ahmadqayum221b@gmail.com>
2026-01-21 19:15:12 +0500

    Initial

---
+++ src/App.js
@@ -0,0 +1,14 @@
+// App.js
+import React from 'react';
+import RabetClone from './rabet_clone.tsx'; // Ensure the path is correct
+import './App.css'; // Where your Tailwind directives live
+
+function App() {
+  return (
+    <div className="App">
+      <RabetClone />
+    </div>
+  );
+}
+
+export default App;
